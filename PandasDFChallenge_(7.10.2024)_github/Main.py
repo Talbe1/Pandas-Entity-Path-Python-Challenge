98d27b5d32b534cfecc9a88a8ec8c476f64fe65b
Talbe <talbarerez@gmail.com>
2024-10-07 15:34:57 +0300

    Added timestamp support. Refactored code.

--- PandasDFChallenge_(7.10.2024)_github/Main.py
+++ PandasDFChallenge_(7.10.2024)_github/Main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from RangedEntitiesFinder import ranged_entity_finder as rEF
 
-# Note: this is a slightly improved version of the previous attempt at this mini challenge.
-
 if __name__ == "__main__":
     # Reading data from Excel into DataFrames.
     total_df = pd.read_excel(r"data/dataTables.xlsx", "total", converters={"id":str})
@@ -13,10 +11,15 @@ if __name__ == "__main__":
     max_distance_from_target = input("Enter max distance from target in km (or enter 's' to stop the program): ")
 
     while max_distance_from_target != 's':
-        try:
-            calcObj.locate_closest_entities_to_target(float(max_distance_from_target))
+        if max_distance_from_target == 's': break
+
+        try: float(max_distance_from_target)
 
         except ValueError:
             print("You didn't enter a float or entered 's'! Please try again.")
+            max_distance_from_target = input("\nEnter max distance from target in km (or enter 's' to stop the program): ")
+            continue
+
+        calcObj.locate_closest_entities_to_target(float(max_distance_from_target))
         
         max_distance_from_target = input("\nEnter max distance from target in km (or enter 's' to stop the program): ")
--- PandasDFChallenge_(7.10.2024)_github/RangedEntitiesFinder.py
+++ PandasDFChallenge_(7.10.2024)_github/RangedEntitiesFinder.py
@@ -7,6 +7,9 @@ class ranged_entity_finder():
     sus_df = pd.DataFrame   # DataFrame that contains data about the path of the target.
 
     def __init__(self, total: pd.DataFrame, sus: pd.DataFrame):
+        if total.shape[1] != 5 or sus.shape[1] != 5:
+            raise ValueError("Table dimensions are incorrect!")
+
         self.total_df = total
         self.sus_df = sus
 
@@ -15,11 +18,8 @@ class ranged_entity_finder():
     on Earth. Each points consists of three values,
     Latitude, Longitude, and height above the surface (in km).
     '''
-    def _calc_distance(self, point_origin: tuple, point_destination: tuple):
-        # Each point must contain 3 coordinate values for the
-        # calculation to work properly.
-        if len(point_origin) != 3 | len(point_destination) != 3:
-            raise ValueError("Point must contain 3 coordinate values!")
+    def _calc_distance(self, point_origin: tuple, point_destination: tuple) -> float:
+        if point_origin == point_destination: return 0
 
         # Saving the latitude and longitude coordinates separately.
         horiz_coords_origin = (point_origin[0], point_origin[1])
@@ -38,21 +38,6 @@ class ranged_entity_finder():
 
 
     '''
-    Returns a point in 3D space representing the coordinates of the current entity whose coordinates belong to.
-    '''
-    def _extract_point_from_row(self, givenRow: pd.DataFrame) -> tuple:
-        # Validating given argument.
-        if (givenRow.shape[0] != 5 or len(givenRow.shape) != 1):
-            raise ValueError("DataFrame for single point data must be a single row and contain 3 coordinate values!")
-
-        # Returning a tuple representing the coordinates.
-        # Turns out that accessing a column in a single row DataFrame
-        # will return the value in that column by itself, not a DataFrame object.
-        # Also there's a conversion to float from Numpy float for easier printing of the actual numbers.
-        return (float(givenRow["lat"]), float(givenRow["long"]), float(givenRow["height"]))
-
-
-    '''
     Locates the closest entities within a defined distance from the **sus**picious target.
     The distance is measured in km.
     '''
@@ -62,96 +47,67 @@ class ranged_entity_finder():
             print("Max distance can't be smaller or equal to 0!")
             return
 
+        # Finding the target Id to drop from total_df for easier navigation in the data.
+        entity_id_to_filter = self.sus_df["id"][0]
+
         # Validating sus table.
-        if len(self.sus_df.groupby(by=["id"])) > 1:
+        if all(self.sus_df["id"] != entity_id_to_filter):
             print("Sus table is invalid!")
             return
 
-        # Grouping the data by the Ids.
-        total_df_grouped = self.total_df.groupby("id")
-
-        # Finding the target Id to drop from total_df for easier navigation in the data.
-        entity_id_to_filter = next(iter(self.sus_df.groupby("id").groups.keys()))
-
         # Removing (dropping) the data about the target from total_df to make working with the data easier.
         # The target might had been dropped since a previous user input during runtime.
-        try: 
-            self.total_df = self.total_df.drop(total_df_grouped.get_group(entity_id_to_filter).index)
+        self.total_df = self.total_df[self.total_df["id"] != entity_id_to_filter]
 
-        except KeyError: pass
+        
+        # Saving occurences of entities crossing paths with the target without timestamp relation.
+        # Grouping according to timestamps is done later.
+        path_crosses = self.total_df.merge(
+            self.sus_df[["lat", "long", "height"]],
+            on=["lat", "long", "height"])
 
-        # Defining an accumulator DataFrame to save any occurences of entities crossing paths with the target entity.
-        pathCrosses = pd.DataFrame();
-
-        # Passing over the rows of the target entity path.
-        for self.sus_df_idx, current_row in self.sus_df.iterrows():
-            # Slicing the DataFrame according to the latitude, longitude, and height.
-            # The coordinates need to match between an entity and the target.
-            # Note2self: remember that the .loc function is written with square brackets ('[' and ']'),
-            #            and NOT regular brackets like in functions. Holy sh*t that syntax is weird.
-            pathCrosses = pd.concat([pathCrosses,
-                      self.total_df.loc[    # < Here's the loc that retreives data from total_df.
-                        # Matching latitude.
-                       (self.total_df["lat"] == current_row["lat"])
-                        # Matching longitude.
-                     & (self.total_df["long"] == current_row["long"])
-                        # Matching height.
-                     & (self.total_df["height"] == current_row["height"])]],
-                      ignore_index = True)
-
-
-        # Storing the coordinates (as points) as a list of tuples, stored in sus DataFrame.
-        path_points_sus = list()
-        for sus_path_idx, current_row in self.sus_df.iterrows():
-
-            try: path_points_sus.append(self._extract_point_from_row(current_row))
-            except ValueError:
-                print("Invalid DataFrame dimensions! Aborting calculations...")
-                return
-
-
-        # Storing the coordinates (as points) as a dictionary of a list of tuples, stored in total DataFrame
-        # (not including the target entity).
-        # Structure of dictionary: keys are points, and the lists contain all entities (without the target) that
-        # were at those coordinates at some point in time.
-        path_points_crosses = dict()
-        for path_crosses_idx, current_row in pathCrosses.iterrows():
-            # Current point on the path of the target.
-            try: current_cross_point = self._extract_point_from_row(current_row)
-            except ValueError:
-                print("Invalid DataFrame dimensions! Aborting calculations...")
-                return
-
-            # Check if the point was already added to the dictionary.
-            if current_cross_point not in path_points_crosses:
-                path_points_crosses[current_cross_point] = list()
-
-            # Adding the entity Id to the dictionary.
-            path_points_crosses[current_cross_point].append(current_row["id"])
+        # Grouping the path crosses according to timestamps (by rising order), latitude, longitude, and height.
+        path_crosses_grouped = path_crosses.groupby(by=["ts", "lat", "long", "height"])[["id"]].apply(dict).dropna()
+        
+        # Storing the coordinates (as points) as a Series of dictionary of tuples, stored in sus DataFrame.
+        path_points_sus = self.sus_df.groupby(by=["ts"])[["lat", "long", "height"]].apply(dict)
 
         print("Format of coordinates: (latitude [deg], longitude [deg], height [km])\n")
 
-        # Iterating over the previously created dictonary (path_points_crosses), whose keys are
-        # points (current_cross_point), and entity Ids list is (current_entities_group).
-        for current_cross_point, current_entities_group in path_points_crosses.items():
-            # Iterating over the points in path of the target.
-            for current_sus_point in path_points_sus:
-                # There's no point in calculating the distance between points if they're the same...
-                if current_sus_point == current_cross_point: continue
+        # Iterating over the path of the target entity.
+        for current_ts in path_points_sus.keys():
+            if current_ts not in path_crosses_grouped.index: continue
 
-                # Catching except for invalid data.
-                try: distance_calculated = self._calc_distance(current_sus_point, current_cross_point)
-                except ValueError:
-                    print("Error! Some data is invalid! A point or more missing 3 coordinate values! Continuing...")
-                    continue
+            # The current group of entities (except target) in the current timestamp.
+            ts_group = path_crosses_grouped.loc[current_ts]
 
-                # Checking if actual distance between entity and target is below the maximum allowed.
-                if distance_calculated < distance_from_target:
-                    print("Entity(ies):", end=' ')
-                    # Note2self: asterisk before an iterable object (such as a list)
-                    #            in a print statement will print all the values in an object
-                    #            in a readable way. Example: [1, 2, 3] with sep=", "
-                    #            will be printed as: 1, 2, 3
-                    print(*current_entities_group, sep=", ", end=' ')
-                    print(f"is/are close to target ({round(distance_calculated, 3)} km)!\n" 
-                          + f"Entity(ies) coordinates:\t{current_cross_point}\nTarget coordinates:\t\t{current_sus_point}\n")
+            # The current location of the target entity (sus).
+            # path_points_sus[current_ts].values() gives a series of values where the left number
+            # is an index and the right is the actual value that's needed.
+            # val.values[0] gets the needed value.
+            sus_location = tuple(float(val.values[0]) for val in path_points_sus[current_ts].values())
+
+            # Previou timestamp is used for checking if the current timestamp was printed already.
+            prev_ts = math.nan
+
+            # Iterating over the group of entities which belong to the current timestamp.
+            for current_entities in ts_group.items():
+                distance_calculated = self._calc_distance(sus_location, current_entities[0])
+
+                if distance_calculated >= distance_from_target: continue
+
+                if prev_ts != current_ts:
+                    print(f"---------- Timestamp {current_ts} ----------")
+                    prev_ts = current_ts
+
+                print(f"Entity(ies):", end=' ')
+
+                # Note2self: asterisk before an iterable object (such as a list)
+                #            in a print statement will print all the values in an object
+                #            in a readable way. Example: [1, 2, 3] with sep=", "
+                #            will be printed as: 1, 2, 3    
+                # Function .values() created a Pandas Series which is turned into a list.
+                # The [0] is used to read only the Ids of the entities, without the idexes of the series.
+                print(*list(current_entities[1].values())[0], sep=", ", end=' ')
+                print(f"is/are close to target ({round(distance_calculated, 3)} km)!\n" 
+                    + f"Entity(ies) coordinates:\t{current_entities[0]}\nTarget coordinates:\t\t{sus_location}\n")
